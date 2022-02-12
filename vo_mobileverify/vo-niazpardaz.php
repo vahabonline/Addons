0150b5a2455e5ebd15e60f1e681e09f864333fa7
Vahab <myvahab@gmail.com>
2022-02-12 14:06:01 +0330

    Add files via upload

---
+++ vo_mobileverify/vo-niazpardaz.php
@@ -0,0 +1,21 @@
+<?php
+function vo_sendSms($params){
+	ini_set("soap.wsdl_cache_enabled", "0");
+	$sms_client = new SoapClient('http://payamak-service.ir/SendService.svc?wsdl', array('encoding'=>'UTF-8'));
+	$usermobiles[] = $params['mobile'];
+	try {
+		$parameters['userName'] = $params['username'];
+		$parameters['password'] = $params['password'];
+		$parameters['fromNumber'] = $params['formnumber'];
+		$parameters['toNumbers'] = $usermobiles;
+		$parameters['messageContent'] = $params['message'];
+		$parameters['isFlash'] = false;
+		$recId = array();
+		$status = array();
+		$parameters['recId'] = &$recId ;
+		$parameters['status'] = &$status ;
+		return $sms_client->SendSMS($parameters)->SendSMSResult;
+	}catch (Exception $e){
+		return 'Caught exception: ',  $e->getMessage(), "\n";
+	}
+}
