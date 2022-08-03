e87b565701809ea0d0eee7b7e457bf5d39d0f24b
Vahab <myvahab@gmail.com>
2022-08-03 09:15:27 +0430

    Update vo-host97.php

--- vo_sms/vo-host97.php
+++ vo_sms/vo-host97.php
@@ -19,15 +19,15 @@ function balance_host97(){
 }
 
 function sending_default_host97($username,$password,$sendernumber,$to,$txt){
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_HEADER, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-	curl_setopt($ch, CURLOPT_URL, "http://sms321.ir/webservice/rest/sms_send?");
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS,   "login_username={$username}&login_password={$password}&receiver_number={$to}&note_arr[]={$txt}&sender_number={$sendernumber}");
-	$results2 = curl_exec($ch);
-	curl_close($curl);
-	return $response;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_URL, 'http://sms.host97.net/webservice/rest/sms_send?');
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS,   "login_username={$username}&login_password={$password}&receiver_number={$to}&note_arr[]={$txt}&sender_number={$sendernumber}");
+    $response = curl_exec($ch);
+    curl_close($curl);
+    return $response;
 }
 
 function sending_pattern_host97($username,$password,$sendernumber,$to,$pattern_code,$patternarray){
