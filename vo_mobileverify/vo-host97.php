40394558a3ad065705938af6f5cbaff1f0caeaa3
Vahab <myvahab@gmail.com>
2022-10-27 08:59:05 +0330

    Create vo-host97.php

---
+++ vo_mobileverify/vo-host97.php
@@ -0,0 +1,17 @@
+<?php
+function vo_sendSms($params){
+  $apiKey = $params['username'];
+  if(empty($apiKey)){
+    $apiKey = $params['password'];
+  }
+  $usermobiles = $params['mobile'];
+  $msg = $params['message'];
+  $sender = $params['formnumber'];
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_HEADER, false);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_URL, 'http://sms.fou.ir/webservice/rest/sms_send?');
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_POSTFIELDS,   'api_key='.$apiKey.'&receiver_number='.$usermobiles.'&note_arr[]='.$msg.'&sender_number='.$sender);
+  $results2 = curl_exec($ch);
+}
