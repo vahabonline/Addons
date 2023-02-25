8879cc7b2bb514e594676fe32e2a74d0c17c16e0
Vahab <myvahab@gmail.com>
2023-02-25 09:00:27 +0330

    Update vo-sabapayamak.php

--- vo_mobileverify/vo-sabapayamak.php
+++ vo_mobileverify/vo-sabapayamak.php
@@ -1,49 +1,26 @@
 <?php
 function vo_sendSms($params){
-    $pAdmin['Username'] = $params['username'];
-    $pAdmin['Password'] = $params['password'];
-    $pAdmin['From'] = $params['formnumber'];
-	return multipleSend($pAdmin,$params['usernumber'], $params['message']);
-}
-
-
-function request($params) {
-    $qryStr = '';
-    foreach ($params as $key => $value) {
-        $qryStr .= '&' . $key . '=' . urlencode($value);
-    }
-    $qryStr = substr($qryStr, 1);
-    $url = 'http://my.sabapayamak.com/API/sendSMS.ashx';
-    $url .= '?' . $qryStr;
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $data = curl_exec($ch);
-    curl_close($ch);
-
-    $results = (!$data ? null : $data );
-
-    return $results;
-}
-
-function multipleSend($pAdmin=array(),$to, $sms_content) {
-
-    $phones = '';
-    foreach ($to as $val) {
-        $phones .= ',' . $val;
-    }
-    $phones = substr($phones, 1);
-
-    $param = array(
-        'username' => $pAdmin['Username'],
-        'password' => $pAdmin['Password'],
-        'to' => $phones,
-        'from' => $pAdmin['From'],
-        'text' => $sms_content
-    );
-
-    $res = request($param);
-
-    return $res;
+	$username = $params['username'];
+	$password = $params['password'];
+	$formnumber = $params['formnumber'];
+	$to = $params['usernumber'];
+	$message = $params['message'];
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+	  CURLOPT_URL => "http://my.sabapayamak.com/API/sendSMS.ashx?username={$username}&password={$password}&to={$to}&from={$formnumber}&text={$message}",
+	  CURLOPT_RETURNTRANSFER => true,
+	  CURLOPT_ENCODING => '',
+	  CURLOPT_MAXREDIRS => 10,
+	  CURLOPT_TIMEOUT => 0,
+	  CURLOPT_FOLLOWLOCATION => true,
+	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	  CURLOPT_CUSTOMREQUEST => 'GET',
+	));
+	$response = curl_exec($curl);
+	curl_close($curl);
+	if(strlen($response) > 5){
+		return 'success';
+	}else{
+		return $response;
+	}
 }
