f311e1a86072a270c3e957a0c6f725e4082e7620
Vahab <myvahab@gmail.com>
2023-03-12 09:34:39 +0330

    Create vo-allsms.php

---
+++ vo_lbm/vo-allsms.php
@@ -0,0 +1,27 @@
+<?php
+function SendMsg($params){
+    $msg = $parameters['messageContent'] = str_replace('{code}',$params['smsCode'],$params['message']);
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://allSMSSend.ir/api/apicore/SendFarapayamak',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS =>'{
+      "UserName": "' . $params['username'] .'",
+      "PassWord": "' . $params['password'] .'",
+      "bodyId": "1",
+      "text": "' . $msg .'",
+      "to": "' . $params['mobile'] .'"
+  }',
+    CURLOPT_HTTPHEADER => array(
+      'Content-Type: application/json'
+    ),
+  ));
+  $response = curl_exec($curl);
+  curl_close($curl);
+	return $response;
+}
