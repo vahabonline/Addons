8aa6313d7a3ae583018c8ed776efe388b672e26a
Vahab <myvahab@gmail.com>
2023-05-23 09:20:47 +0330

    Update vo-idehpayam.php

--- vo_reglogin/gateways/vo-idehpayam.php
+++ vo_reglogin/gateways/vo-idehpayam.php
@@ -1,6 +1,7 @@
 <?php
 function vo_sendSms($params){
 	$curl = curl_init();
+
 	curl_setopt_array($curl, array(
 	  CURLOPT_URL => 'http://185.112.33.62/api/v1/rest/sms/send',
 	  CURLOPT_RETURNTRANSFER => true,
@@ -10,21 +11,29 @@ function vo_sendSms($params){
 	  CURLOPT_FOLLOWLOCATION => true,
 	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 	  CURLOPT_CUSTOMREQUEST => 'POST',
-	  CURLOPT_POSTFIELDS => array('recipients' => [$params['usermobile']],'message' => $params['message']),
+	  CURLOPT_POSTFIELDS =>'{
+	    "from": $params['sendernumber'],
+	    "recipients": [$params['usermobile']],
+	    "message": $params['message'],
+	    "type": 0
+	}',
 	  CURLOPT_HTTPHEADER => array(
-		'Content-type: application/json',
-		'username: '.$params['username'],
-		'password: '.$params['password']
+	    'username: '.$params['username'],
+	    'password: '.$params['username'],
+	    'Content-Type: application/json'
 	  ),
 	));
+
 	$response = curl_exec($curl);
 	curl_close($curl);
 	$res = json_decode($response);
-    if($res->status == '200'){
+	$params['status'] = false;
+	if($res->status == '200'){
 		if($res->result->status == '0'){
 			$params['status'] = true;
 		}
-    }
-    $params['result'] = $response;
-    return $params;
+	    }
+	    $params['result'] = $response;
+	    return $params;
+
 }
