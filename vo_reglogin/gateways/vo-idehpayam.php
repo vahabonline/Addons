4ef974cbc9d10bfd411914e118a6a065f62ce2f4
Vahab <myvahab@gmail.com>
2023-05-23 13:47:21 +0330

    Update vo-idehpayam.php

--- vo_reglogin/gateways/vo-idehpayam.php
+++ vo_reglogin/gateways/vo-idehpayam.php
@@ -1,35 +1,39 @@
 <?php
 function vo_sendSms($params){
-	$username = $params['username'];
+    $username = $params['username'];
 	$password = $params['password'];
 	$sendernumber = $params['sendernumber'];
 	$usermobile = $params['usermobile'];
 	$message = $params['message'];
+	
 	$curl = curl_init();
-	curl_setopt_array($curl, array(
-	  CURLOPT_URL => 'http://185.112.33.62/api/v1/rest/sms/send',
-	  CURLOPT_RETURNTRANSFER => true,
-	  CURLOPT_ENCODING => '',
-	  CURLOPT_MAXREDIRS => 10,
-	  CURLOPT_TIMEOUT => 0,
-	  CURLOPT_FOLLOWLOCATION => true,
-	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	  CURLOPT_CUSTOMREQUEST => 'POST',
-	  CURLOPT_POSTFIELDS =>'{
-	    "from": "'.$sendernumber.'",
-	    "recipients": "['.$usermobile.']",
-	    "message": "'.$message'",
-	    "type": 0
-	}',
-	  CURLOPT_HTTPHEADER => array(
-	    'username: '.$username,
-	    'password: '.$password,
-	    'Content-Type: application/json'
-	  ),
-	));
-
-	$response = curl_exec($curl);
-	curl_close($curl);
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => 'http://185.112.33.62/api/v1/rest/sms/send',
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'POST',
+      CURLOPT_POSTFIELDS =>'{
+        "from": "'.$sendernumber.'",
+        "recipients": [
+            "'.$usermobile.'"
+        ],
+        "message": "'.$message.'",
+        "type": 0
+    }',
+      CURLOPT_HTTPHEADER => array(
+        'username: '.$username,
+        'password: '.$password,
+        'Content-Type: application/json'
+      ),
+    ));
+    
+    $response = curl_exec($curl);
+    
+    curl_close($curl);
 	$res = json_decode($response);
 	$params['status'] = false;
 	if($res->status == '200'){
