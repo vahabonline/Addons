4ae7f18ab62003506d882f9f5583f17f818c6971
Vahab <myvahab@gmail.com>
2023-05-31 09:27:50 +0330

    Update vo-niksms.php

--- vo_lbm/lib/Senders/vo-niksms.php
+++ vo_lbm/lib/Senders/vo-niksms.php
@@ -1,19 +1,35 @@
 <?php
 
-$curl = curl_init();
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://niksms.com/fa/publicapi/ptpSms?username=YourUsername&password=YourPassword&senderNumber=**********&numbers=**********%2C***********&sendOn=2018%2F01%2F04-10%3A00&sendType=1&yourMessageIds=1001%2C1002&message=Here%20You%20Should%20Put%20Your%20First%20Message%20Text%2C%20Here%20You%20should%20put%20your%20second%20message%20text',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-));
+function vo_sms_sending($vars){
+	  $username = $vars['panel_username'];
+    $password = $vars['panel_password'];
+    $apikey = $vars['panel_password'];
+    $sendernumber = $vars['panel_sendernumber'];
+    $phonenumber = $vars['sec_usernumber'];
+    $message = $vars['msgtxt'];
+    $now = date('Y/m/d-H:i:s');
 
-$response = curl_exec($curl);
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://niksms.com/fa/publicapi/ptpSms?username={$username}&password={$password}&senderNumber={$sendernumber}&numbers={$phonenumber}&sendType=1&message=".urlencode($message),
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'GET',
+    ));
+    $response = curl_exec($curl);
+    curl_close($curl);
+    $res = json_decode($response,true);
+    if($res['status'] == 0){
+      return 'success';
+    }
+    return $response;
+  
 
-curl_close($curl);
-echo $response;
+}
+
+
