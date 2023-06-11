77d83448016a92580333b3c02359ea2d891d658f
Vahab <myvahab@gmail.com>
2023-06-11 02:05:40 -0700

    Add files via upload

---
+++ vo_bankpay/lib/Senders/vo-idehpayam.php
@@ -0,0 +1,48 @@
+<?php
+
+function send_sms($params){
+    ini_set("soap.wsdl_cache_enabled", "0");
+    try {
+
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => 'http://185.112.33.62/api/v1/rest/sms/send',
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => '',
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 0,
+		  CURLOPT_FOLLOWLOCATION => true,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => 'POST',
+		  CURLOPT_POSTFIELDS =>'{
+			"from": "'.$params['sendnumber'].'",
+			"recipients": [
+				"'.$params['tonumbers'].'"
+			],
+			"message": "'.$params['message'].'",
+			"type": 0
+		}',
+		  CURLOPT_HTTPHEADER => array(
+			'username: '.$params['username'],
+			'password: '.$params['password'],
+			'Content-Type: application/json'
+		  ),
+		));
+
+		$response = curl_exec($curl);
+
+		curl_close($curl);
+		return $response;
+		
+		
+		
+		
+		
+		
+		
+		
+    } catch (SoapFault $ex) {
+        return $ex->faultstring;
+    }
+}
