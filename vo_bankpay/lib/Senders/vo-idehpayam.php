12b9298d9a2d36fa95d89ff2166fb31725bcd2b9
Vahab <myvahab@gmail.com>
2023-06-14 12:52:03 +0330

    Update vo-idehpayam.php

--- vo_bankpay/lib/Senders/vo-idehpayam.php
+++ vo_bankpay/lib/Senders/vo-idehpayam.php
@@ -3,44 +3,42 @@
 function send_sms($params){
     ini_set("soap.wsdl_cache_enabled", "0");
     try {
+	    
+	$from = $params['sendnumber'];
+	$to = $params['tonumbers'];
+	$message = $params['message'];
+	$username = $params['username'];
+	$password = $params['password'];
+	    
+    	$curl = curl_init();
+	curl_setopt_array($curl, array(
+	  CURLOPT_URL => 'http://185.112.33.62/api/v1/rest/sms/send',
+	  CURLOPT_RETURNTRANSFER => true,
+	  CURLOPT_ENCODING => '',
+	  CURLOPT_MAXREDIRS => 10,
+	  CURLOPT_TIMEOUT => 0,
+	  CURLOPT_FOLLOWLOCATION => true,
+	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	  CURLOPT_CUSTOMREQUEST => 'POST',
+	  CURLOPT_POSTFIELDS =>'{
+	    "from": "'.$from.'",
+	    "recipients": [
+		"'.$to.'"
+	    ],
+	    "message": "'.$message.'",
+	    "type": 0
+	}',
+	  CURLOPT_HTTPHEADER => array(
+	    'username: '.$username,
+	    'password: '.$password,
+	    'Content-Type: application/json'
+	  ),
+	));
 
-		$curl = curl_init();
+	$response = curl_exec($curl);
 
-		curl_setopt_array($curl, array(
-		  CURLOPT_URL => 'http://185.112.33.62/api/v1/rest/sms/send',
-		  CURLOPT_RETURNTRANSFER => true,
-		  CURLOPT_ENCODING => '',
-		  CURLOPT_MAXREDIRS => 10,
-		  CURLOPT_TIMEOUT => 0,
-		  CURLOPT_FOLLOWLOCATION => true,
-		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST => 'POST',
-		  CURLOPT_POSTFIELDS =>'{
-			"from": "'.$params['sendnumber'].'",
-			"recipients": [
-				"'.$params['tonumbers'].'"
-			],
-			"message": "'.$params['message'].'",
-			"type": 0
-		}',
-		  CURLOPT_HTTPHEADER => array(
-			'username: '.$params['username'],
-			'password: '.$params['password'],
-			'Content-Type: application/json'
-		  ),
-		));
-
-		$response = curl_exec($curl);
-
-		curl_close($curl);
-		return $response;
-		
-		
-		
-		
-		
-		
-		
+	curl_close($curl);
+	return $response;
 		
     } catch (SoapFault $ex) {
         return $ex->faultstring;
