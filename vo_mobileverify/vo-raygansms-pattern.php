0747674d794f0a52a4211a8f9fc0c25ab5f538c8
Vahab <myvahab@gmail.com>
2023-11-09 08:48:47 +0330

    Update vo-raygansms-pattern.php

--- vo_mobileverify/vo-raygansms-pattern.php
+++ vo_mobileverify/vo-raygansms-pattern.php
@@ -1,1 +1,29 @@
+<?php
 
+function vo_sendSms($params){
+    try {
+        $Url="http://smspanel.Trez.ir/SendMessageWithCode.ashx";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$Url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, 
+        http_build_query(
+          array(
+            'Username' => $params['usernumber'],
+            'Password  $params['password'],
+            'Mobile' => $params['usernumber'],
+            'Message' => $params['message']
+          )));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_output = curl_exec($ch);
+        curl_close ($ch);
+        return 'success';
+    }catch(Exception $e) {
+      return 'Message: ' .$e->getMessage();
+    }
+
+
+        
+}
+
+?> 
