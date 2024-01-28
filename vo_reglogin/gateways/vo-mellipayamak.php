333cf15100a6ab67c05b26c080f741832c3057c0
Vahab <myvahab@gmail.com>
2024-01-28 17:56:40 +0330

    Create vo-mellipayamak.php

---
+++ vo_reglogin/gateways/vo-mellipayamak.php
@@ -0,0 +1,21 @@
+<?php
+function vo_sendSms($params){
+    $username = $params['username'];
+	$password = $params['password'];
+	$from = $params['sendernumber'];
+	$to[] = $params['usermobile'];
+	$message = $params['message'];
+	
+
+	ini_set("soap.wsdl_cache_enabled",0);
+	$sms = new SoapClient("http://api.payamak-panel.com/post/Send.asmx?wsdl",array("encoding"=>"UTF-8"));
+	$data = array(
+		"username"	=>	$username,
+		"password"	=>	$password,
+		"to"	=>	$to,
+		"from"	=>	$from,
+		"text"	=>	$message,
+		"isflash"	=>	false
+	);
+	return $sms->SendSimpleSMS($data)->SendSimpleSMSResult;
+}
