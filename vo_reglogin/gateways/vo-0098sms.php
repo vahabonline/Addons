3c1dad749c1dec0f77845a0979de7d77abde4e57
Vahab <myvahab@gmail.com>
2024-02-04 07:56:26 +0330

    Update vo-0098sms.php

--- vo_reglogin/gateways/vo-0098sms.php
+++ vo_reglogin/gateways/vo-0098sms.php
@@ -3,14 +3,15 @@ function vo_sendSms($params){
 	$sms_client = new SoapClient('http://webservice.0098sms.com/service.asmx?wsdl', array('encoding'=>'UTF-8'));
 	$parameters['username'] = $params['username'];
 	$parameters['password'] = $params['password'];
-	$parameters['mobileno'] = $params['usermobile'];
+	$parameters['mobileno'] = '0' . $params['usermobile'];
 	$parameters['pnlno'] = $params['sendernumber'];
 	$parameters['text'] = $params['message'];
 	$parameters['isflash'] = false;
 	$res = $sms_client->SendSMS($parameters)->SendSMSResult;
-	$params['status'] = false;
-	if(strlen($res->string) > 5){
+	if($res == '2'){
 	    $params['status'] = true;
+	}else{
+	    $params['status'] = false;
 	}
 	$params['result'] = $res;
 	return $params;
