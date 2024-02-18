eff8520ea56cb429bdbfbb41a761891f2438f679
Vahab <myvahab@gmail.com>
2024-02-18 14:38:02 +0330

    Update elanak.php

--- vo_uiv/lib/Senders/elanak.php
+++ vo_uiv/lib/Senders/elanak.php
@@ -4,7 +4,7 @@ function sendSms($vars){
   	$soap->Username=$vars['UsernameOrApiKey'];
   	$soap->Password=$vars['PasswordOrSecretkey'];
   	$soap->fromNum=$vars['sendernumber'];
-  	$soap->toNum=array($vars['mobile']);
+  	$soap->toNum=$vars['mobile'];
   	$soap->Content = $vars['message'];
   	$soap->Type = '0';
   	$array = $soap->SendSMS($soap->fromNum,$soap->toNum,$soap->Content,$soap->Type,$soap->Username,$soap->Password);
