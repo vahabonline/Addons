3948fb8f9c1cd3455facffe8f6fea18445c69ee9
Vahab <myvahab@gmail.com>
2024-03-23 09:58:12 +0330

    Create vo-kavehnegarSms.php

---
+++ vo_lbm/lib/Senders/vo-kavehnegarSms.php
@@ -0,0 +1,20 @@
+<?php
+  $curl = curl_init();
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => 'http://api.kavenegar.com/v1/'.$vars['panel_username'].'/sms/send.json',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => 'receptor='.$vars['sec_usernumber'].'&sender='.$vars['panel_sendernumber'].'&message=' . urlencode($vars['msgtxt']),
+    CURLOPT_HTTPHEADER => array(
+      'Content-Type: application/x-www-form-urlencoded'
+    ),
+  ));
+  $response = curl_exec($curl);
+  curl_close($curl);
+  return $response;
+}
