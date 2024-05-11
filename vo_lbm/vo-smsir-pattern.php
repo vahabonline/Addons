bdf84547282312cd2f00f417c9359ee182243479
Vahab <myvahab@gmail.com>
2024-05-11 14:14:04 +0330

    Create vo-smsir-pattern.php

---
+++ vo_lbm/vo-smsir-pattern.php
@@ -0,0 +1,35 @@
+<?php
+function vo_sms_sending($vars){
+	      $curl = curl_init();
+
+      curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://api.sms.ir/v1/send/verify',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS =>'{
+        "mobile": '.$vars['sec_usernumber'].',
+        "templateId": '.$vars['otpid'].',
+        "parameters": [
+          {
+            "name": "code",
+            "value": "'.$vars['sec_code'].'"
+          }
+        ]
+      }',
+        CURLOPT_HTTPHEADER => array(
+          'Content-Type: application/json',
+          'Accept: text/plain',
+          'x-api-key: ' . $vars['panel_password']
+        ),
+      ));
+
+      $response = curl_exec($curl);
+
+      curl_close($curl);
+      return $response;
+}
