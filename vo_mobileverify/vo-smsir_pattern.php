e29f7f8b1e84f8ce5651f7a8079eebc2162b0aec
Vahab <myvahab@gmail.com>
2024-06-10 15:46:00 +0330

    Update vo-smsir_pattern.php

--- vo_mobileverify/vo-smsir_pattern.php
+++ vo_mobileverify/vo-smsir_pattern.php
@@ -1,177 +1,50 @@
 <?php
-class SmsIR_UltraFastSend
-{
-
-    /**
-     * Gets API Ultra Fast Send Url.
-     *
-     * @return string Indicates the Url
-     */
-    protected function getAPIUltraFastSendUrl() 
-    {
-        return "api/UltraFastSend";
-    }
-
-    /**
-     * Gets Api Token Url.
-     *
-     * @return string Indicates the Url
-     */
-    protected function getApiTokenUrl()
-    {
-        return "api/Token";
-    }
-
-    /**
-     * Gets config parameters for sending request.
-     *
-     * @param string $APIKey    API Key
-     * @param string $SecretKey Secret Key
-     * @param string $APIURL    API URL
-     * 
-     * @return void
-     */
-    public function __construct($APIKey, $SecretKey, $APIURL)
-    {
-        $this->APIKey = $APIKey;
-        $this->SecretKey = $SecretKey;
-        $this->APIURL = $APIURL;
-    }
-
-    /**
-     * Ultra Fast Send Message.
-     *
-     * @param data[] $data array structure of message data
-     * 
-     * @return string Indicates the sent sms result
-     */
-    public function ultraFastSend($data) 
-    {
-        $token = $this->_getToken($this->APIKey, $this->SecretKey);
-        if ($token != false) {
-            $postData = $data;
-
-            $url = $this->APIURL.$this->getAPIUltraFastSendUrl();
-            $UltraFastSend = $this->_execute($postData, $url, $token);
-            $object = json_decode($UltraFastSend);
-
-            $result = false;
-            if (is_object($object)) {
-                $result = $object->Message;
-            } else {
-                $result = false;
-            }
-        } else {
-            $result = false;
-        }
-        return $result;
-    }
-
-    /**
-     * Gets token key for all web service requests.
-     *
-     * @return string Indicates the token key
-     */
-    private function _getToken()
-    {
-        $postData = array(
-            'UserApiKey' => $this->APIKey,
-            'SecretKey' => $this->SecretKey,
-            'System' => 'php_rest_v_2_0'
-        );
-        $postString = json_encode($postData);
-
-        $ch = curl_init($this->APIURL.$this->getApiTokenUrl());
-        curl_setopt(
-            $ch, CURLOPT_HTTPHEADER, array(
-                'Content-Type: application/json'
-            )
-        );
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
-
-        $result = curl_exec($ch);
-        curl_close($ch);
-
-        $response = json_decode($result);
-
-        $resp = false;
-        $IsSuccessful = '';
-        $TokenKey = '';
-        if (is_object($response)) {
-            $IsSuccessful = $response->IsSuccessful;
-            if ($IsSuccessful == true) {
-                $TokenKey = $response->TokenKey;
-                $resp = $TokenKey;
-            } else {
-                $resp = false;
-            }
-        }
-        return $resp;
-    }
-
-    /**
-     * Executes the main method.
-     *
-     * @param postData[] $postData array of json data
-     * @param string     $url      url
-     * @param string     $token    token string
-     * 
-     * @return string Indicates the curl execute result
-     */
-    private function _execute($postData, $url, $token)
-    {
-        $postString = json_encode($postData);
-
-        $ch = curl_init($url);
-        curl_setopt(
-            $ch, CURLOPT_HTTPHEADER, array(
-                'Content-Type: application/json',
-                'x-sms-ir-secure-token: '.$token
-            )
-        );
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
-
-        $result = curl_exec($ch);
-        curl_close($ch);
-
-        return $result;
-    }
-}
-
 function vo_sendSms($params){
     try {
-          $json = html_entity_decode($params['message']);
-          $json = json_decode($json);
-          $codename = $json->codename;
-            $pattern_code = $json->patterncode;
-    	$actcode = $json->activecode;
-    	$usermob = $params['usernumber'];
-          $APIKey = $params['username'];
-          $SecretKey = $params['password'];
-          $APIURL = "https://ws.sms.ir/";
-      $data = array(
-          "ParameterArray" => array(
-              array(
-                  "Parameter" => $json->codename,
-                  "ParameterValue" => $json->activecode
-              )
-          ),
-          "Mobile" => $params['usernumber'],
-          "TemplateId" => $pattern_code
-      );
-  
-      $SmsIR_UltraFastSend = new SmsIR_UltraFastSend($APIKey, $SecretKey, $APIURL);
-      $UltraFastSend = $SmsIR_UltraFastSend->ultraFastSend($data);
-      return 'success';
+        
+        $jsonString = html_entity_decode(htmlspecialchars_decode($params['message']));
+        $json = json_decode($jsonString);
+        $codename = $json->codename;
+        $pattern_code = $json->patterncode;
+        $actcode = $json->activecode;
+        $usermob = $params['usernumber'];
+        $APIKey = $params['username'];
+        $SecretKey = $params['password'];
+          
+        $curl = curl_init();
+        $params = [
+            'mobile' => $params['usernumber'],
+            'templateId' => $pattern_code,
+            'parameters' => [
+                [
+                    'name' => $codename,
+                    'value' => $actcode
+                ]
+            ]
+        ];
+          curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.sms.ir/v1/send/verify',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => json_encode($params),
+            CURLOPT_HTTPHEADER => array(
+              'Content-Type: application/json',
+              'Accept: text/plain',
+              'x-api-key: ' . $SecretKey
+            ),
+          ));
+    
+          $response = curl_exec($curl);
+    
+          curl_close($curl);
+          return $response;
+            return 'success';
     }catch(Exception $e) {
       return 'Message: ' .$e->getMessage();
     }
 }
-
-?> 
