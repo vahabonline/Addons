77f7ffcd17a41b2b38ad0750f8d36cf9f5ef1489
Vahab <myvahab@gmail.com>
2024-07-08 16:19:25 +0330

    Update vo-kavehnegar.php

--- vo_sms/vo-kavehnegar.php
+++ vo_sms/vo-kavehnegar.php
@@ -1,7 +1,7 @@
 <?php
 function info_kavehnegar(){
     return array(
-        "name" => "KavehNegar",
+        "name" => "IpPanel",
         "username_label" => "توکن",
         "password_label" => false,
 		"sendernumber_label" => "شماره ارسال کننده",
@@ -12,69 +12,71 @@ function info_kavehnegar(){
 
 // وضعیت اتصال
 function status_kavehnegar($ApiKey,$pass,$sender){
-	$curl = curl_init();
-	curl_setopt_array($curl, array(
-	  CURLOPT_URL => "https://api.kavenegar.com/v1/{$ApiKey}/account/config.json?defaultsender={$sender}&apilogs=justfaults&debugmode=enabled",
-	  CURLOPT_RETURNTRANSFER => true,
-	  CURLOPT_ENCODING => '',
-	  CURLOPT_MAXREDIRS => 10,
-	  CURLOPT_TIMEOUT => 0,
-	  CURLOPT_FOLLOWLOCATION => true,
-	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	  CURLOPT_CUSTOMREQUEST => 'GET',
-	));
-	$response = curl_exec($curl);
-	curl_close($curl);
-	$response = json_decode($response);
-	if($response->return->status == '200'){
-		return true;
-	}else{
-		return false;
+	try{
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => "https://api.kavenegar.com/v1/{$ApiKey}/account/config.json?defaultsender={$sender}&apilogs=justfaults",
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'GET',
+		));
+		$response = curl_exec($curl);
+		curl_close($curl);
+		return $response;
+	}catch(Exception $e) {
+		return $e->getMessage();
 	}
 }
 
 // نمایش اعتبار پنل
 function balance_kavehnegar($ApiKey,$pass,$sender){
-	$curl = curl_init();
-	curl_setopt_array($curl, array(
-	  CURLOPT_URL => "https://api.kavenegar.com/v1/{$ApiKey}/account/config.json?defaultsender={$sender}&apilogs=justfaults&debugmode=enabled",
-	  CURLOPT_RETURNTRANSFER => true,
-	  CURLOPT_ENCODING => '',
-	  CURLOPT_MAXREDIRS => 10,
-	  CURLOPT_TIMEOUT => 0,
-	  CURLOPT_FOLLOWLOCATION => true,
-	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	  CURLOPT_CUSTOMREQUEST => 'GET',
-	));
-	$response = curl_exec($curl);
-	curl_close($curl);
-	$response = json_decode($response);
-	if($response->return->status == '200'){
-		return $response->entries->mincreditalarm;
-	}else{
-		return json_encode($response);
+	try{
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => "https://api.kavenegar.com/v1/{$ApiKey}/account/config.json?defaultsender={$sender}&apilogs=justfaults",
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'GET',
+		));
+		$response = curl_exec($curl);
+		curl_close($curl);
+		return $response;
+	}catch(Exception $e) {
+		return $e->getMessage();
 	}
 }
 
 // ارسال عادی
 function sending_default_kavehnegar($apiKey,$pass,$fromnumber,$usermobile,$message){
-	$curl = curl_init();
-	curl_setopt_array($curl, array(
-	  CURLOPT_URL => "https://api.kavenegar.com/v1/{$apiKey}/sms/send.json?receptor={$usermobile}&sender={$fromnumber}&message={$message}",
-	  CURLOPT_RETURNTRANSFER => true,
-	  CURLOPT_ENCODING => '',
-	  CURLOPT_MAXREDIRS => 10,
-	  CURLOPT_TIMEOUT => 0,
-	  CURLOPT_FOLLOWLOCATION => true,
-	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	  CURLOPT_CUSTOMREQUEST => 'GET',
-	));
-	$response = curl_exec($curl);
-	curl_close($curl);
-	return $response;
+	try{
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => "https://api.kavenegar.com/v1/{$apiKey}/sms/send.json?receptor={$usermobile}&sender={$fromnumber}&message={$message}",
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'GET',
+		));
+		$response = curl_exec($curl);
+		curl_close($curl);
+		return $response;
+	}catch(Exception $e) {
+		return $e->getMessage();
+	}
 }
 
 // ارسال به صورت پترن
 function sending_pattern_kavehnegar($username,$password,$sender,$to,$pattern_code,$msg){
-	return false;
+	return false
 }
