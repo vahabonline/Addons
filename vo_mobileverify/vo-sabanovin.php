a7fcf1f3a88ac78b6808fb22735fae81ff915cc0
Vahab <myvahab@gmail.com>
2024-07-25 10:47:52 +0330

    Update vo-sabanovin.php

--- vo_mobileverify/vo-sabanovin.php
+++ vo_mobileverify/vo-sabanovin.php
@@ -1,30 +1,29 @@
 <?php
 
-vo_sendSms($params){
-  $apikey = $params['username'];
-  if(empty()){
-    $apikey = $params['password'];
-  }
-  $from = $params['formnumber'];
-  $to = $params['usernumber'];
-  $msg = $params['message'];
-  $curl = curl_init();
-  curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://api.sabanovin.com/v1/{$apikey}/sms/send.json?gateway={$from}&to={$to}&text={$msg}",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'GET',
-  ));
-  $response = curl_exec($curl);
-  curl_close($curl);
-  $res = json_decode($response);
-  if($res['status']['code'] == 200){
-    return 'success';
-  }else{
-    return $response;
-  }
+function vo_sendSms($params){
+	$apiKey = $params['password'];
+    $curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => "https://api.sabanovin.com/v1/{$apiKey}/sms/send.json",
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'POST',
+		CURLOPT_POSTFIELDS => array(
+			'gateway' => $params['formnumber'],
+			'to' => $params['mobile'],
+			'text' => $params['message']
+		),
+	));
+	$response = curl_exec($curl);
+	curl_close($curl);
+	$result = json_decode($response, true);
+	if($result['status']['code'] == '200'){
+        return 'success';
+    }else{
+        return response;
+    }
 }
