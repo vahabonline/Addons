912aabff24fdeaac8b9ab7bd97b15482a7245ca7
Vahab <myvahab@gmail.com>
2024-09-09 08:32:51 +0330

    Create vo-ippanel.php

---
+++ vo_mobileverify/vo-ippanel.php
@@ -0,0 +1,24 @@
+<?php
+function vo_sendSms($params){
+    $url = "https://ippanel.com/services.jspd";
+    $param = array(
+        'uname'=>$params['username'],
+        'pass'=>$params['password'],
+        'from'=>$params['formnumber'],
+        'message'=>$params['message'],
+        'to'=>array($params['usernumber']),
+        'op'=>'send'
+    );
+    $handler = curl_init($url);
+    curl_setopt($handler, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($handler, CURLOPT_POSTFIELDS, $param);
+    curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($handler);
+    $response = json_decode($result);
+    $res_code = $response[0];
+    if($res_code == '0'){
+        return 'success';
+    }else{
+        return $result;
+    }
+}
