e409a1647f73c692c00382358ec695b6120c68a4
Vahab <myvahab@gmail.com>
2024-12-16 10:20:39 +0330

    Update vo-sahandsms.php

--- vo_sms/vo-sahandsms.php
+++ vo_sms/vo-sahandsms.php
@@ -26,26 +26,25 @@ function balance_sahandsms($user,$pass,$sender){
 
 // ارسال عادی
 function sending_default_sahandsms($username,$password,$from,$to,$txt){
-		$to = explode(",",$to);
-		try {
-      $toNumber = $params['mobile'];
-      $url = 'http://www.sahandsms.com/NewWebService/newSMSWebService.asmx?wsdl';
-      $client = new SoapClient($url);
-      if(is_array($toNumber)){
-          $toNumber = $to;
-      }else{
-          $toNumber = array($to);
-      }
-      $parameters['username'] = $username;
-      $parameters['password'] = $password;
-      $parameters['message'] = $txt;
-      $parameters['fromNumber'] = '+98'.$from;
-      $parameters['toNumbers'] = $toNumber;
-      $return_val = $client->SendQeue($parameters);
-      return json_encode($return_val);
-		} catch (SoapFault $ex) {
-			return $ex->faultstring;
-		}
+	$to = explode(",",$to);
+	try {
+	      $toNumber = $params['mobile'];
+	      $url = 'http://www.sahandsms.com/NewWebService/newSMSWebService.asmx?wsdl';
+	      $client = new SoapClient($url);
+	      if(is_array($toNumber)){
+	          $toNumber = $to;
+	      }else{
+	          $toNumber = array($to);
+	      }
+	      $parameters['username'] = $username;
+	      $parameters['password'] = $password;
+	      $parameters['message'] = $txt;
+	      $parameters['fromNumber'] = '+98'.$from;
+	      $parameters['toNumbers'] = $toNumber;
+	      $return_val = $client->SendQeue($parameters);
+      		return json_encode($return_val);
+	} catch (SoapFault $ex) {
+		return $ex->faultstring;
 	}
 }
 
