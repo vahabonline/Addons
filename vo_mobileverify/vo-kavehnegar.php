052b3472491594fbfdcf3eb141388e576d1ee172
Vahab <myvahab@gmail.com>
2024-12-29 16:38:58 +0330

    Update vo-kavehnegar.php

--- vo_mobileverify/vo-kavehnegar.php
+++ vo_mobileverify/vo-kavehnegar.php
@@ -1,37 +1,34 @@
 <?php
+
 function vo_sendSms($params){
-	$ApiKey = $params['username'];
+    	$token = $params['username'];
 	if(empty($ApiKey)){
-		$ApiKey = $params['password'];
+		$token = $params['password'];
 	}
-	
-	$formnumber = $params['formnumber'];
-	$message = $params['message'];
-	$usernumber = $params['usernumber'];
-
-	
+	$sendernumber = $params['formnumber'];
+	$usermobile = $params['usernumber'];
+	$message = urlencode($params['message']);
 	$curl = curl_init();
-
 	curl_setopt_array($curl, array(
-	  CURLOPT_URL => "https://api.kavenegar.com/v1/{$ApiKey}/sms/send.json?receptor={$usernumber}&sender={$formnumber}&message={$message}",
-	  CURLOPT_RETURNTRANSFER => true,
-	  CURLOPT_ENCODING => '',
-	  CURLOPT_MAXREDIRS => 10,
-	  CURLOPT_TIMEOUT => 0,
-	  CURLOPT_FOLLOWLOCATION => true,
-	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	  CURLOPT_CUSTOMREQUEST => 'GET',
+	CURLOPT_URL => "http://api.kavenegar.com/v1/{$token}/sms/send.json",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => '',
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => 'POST',
+	CURLOPT_POSTFIELDS => "receptor={$usermobile}&sender={$sendernumber}&message={$message}",
+	CURLOPT_HTTPHEADER => array(
+		'Content-Type: application/x-www-form-urlencoded',
+		'Cookie: cookiesession1=678A8C31FHJLMNOQRSTUV012345665A2'
+	  ),
 	));
-
 	$response = curl_exec($curl);
 	curl_close($curl);
-	
-	if($response){
-		$json = json_decode($response);
-		if($json['return']['status'] == 200){
-			return 'success';
-		}
+	$res = json_decode($response);
+	if($res->return->status == '200'){
+		return 'success';
 	}
-
 	return $response;
 }
