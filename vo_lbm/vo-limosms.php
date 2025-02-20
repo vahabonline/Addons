3a2aa01048bb543351b76d0f0d560af24cafa836
Vahab <myvahab@gmail.com>
2025-02-20 09:25:52 +0330

    Create vo-limosms.php

---
+++ vo_lbm/vo-limosms.php
@@ -0,0 +1,28 @@
+<?php
+
+
+function vo_sms_sending($params){
+  $url ='https://api.limosms.com/api/sendsms';
+  $receiver[] = $vars['sec_usernumber'];
+  $post_data = json_encode(array(
+  'Message' =>$vars['msgtxt'] ,
+  'SenderNumber' => $vars['panel_sendernumber'],
+  'MobileNumber' => $receiver,
+  ));
+  $process = curl_init();
+  curl_setopt( $process,CURLOPT_URL,$url);
+  curl_setopt( $process, CURLOPT_TIMEOUT,30);
+  curl_setopt( $process, CURLOPT_POST, 1);
+  curl_setopt($process, CURLOPT_CUSTOMREQUEST, "POST");
+  curl_setopt( $process, CURLOPT_POSTFIELDS, $post_data);
+  curl_setopt( $process, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt( $process, CURLOPT_FOLLOWLOCATION, true);
+  curl_setopt( $process, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'ApiKey:' . $vars['panel_password']
+  ));
+  $return = curl_exec( $process);
+  $httpcode = curl_getinfo( $process, CURLINFO_HTTP_CODE);
+  curl_close($process);
+  return $return;
+}
