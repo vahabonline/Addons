6836564f706274ae4fb74f64e5c439e1aad1d589
Vahab <myvahab@gmail.com>
2025-03-05 08:30:19 +0330

    Update vo-limosms.php

--- vo_sms/vo-limosms.php
+++ vo_sms/vo-limosms.php
@@ -24,9 +24,9 @@ function info_limosms(){
         "name" => "limosms",
         "username_label" => "توکن",
         "password_label" => "توکن",
-		"sendernumber_label" => "شماره ارسال کننده",
-		"pattern_label" => false,
-        "pattern" => false,
+	"sendernumber_label" => "شماره ارسال کننده",
+	"pattern_label" => 'شماره ارسال کننده خدماتی',
+        "pattern" => true,
     );
 }
 
@@ -69,5 +69,27 @@ function sending_default_limosms($user,$password,$from,$to,$txt){
 
 // ارسال به صورت پترن
 function sending_pattern_limosms($username,$password,$fromNum,$to,$pattern_code,$msg){
-	return false;
+	$url ='https://api.limosms.com/api/sendpatternmessage';
+	$post_data = json_encode(array(
+		'OtpId' => $pattern_code,
+		'ReplaceToken' => json_decode($msg),
+		'MobileNumber' => $to
+	));
+	$process = curl_init();
+	curl_setopt( $process,CURLOPT_URL,$url);
+	curl_setopt( $process, CURLOPT_TIMEOUT,30);
+	curl_setopt( $process, CURLOPT_POST, 1);
+	curl_setopt($process, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt( $process, CURLOPT_POSTFIELDS, $post_data);
+	curl_setopt( $process, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt( $process, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt( $process, CURLOPT_HTTPHEADER, array(
+		'Content-Type: application/json',
+		'ApiKey:' . $username
+	));
+	$return = curl_exec( $process);
+	$httpcode = curl_getinfo( $process, CURLINFO_HTTP_CODE);
+	curl_close($process);
+	$decoded = json_decode($return);
+	return $decoded;
 }
