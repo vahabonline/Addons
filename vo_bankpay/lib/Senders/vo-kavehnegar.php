e8f96949025ceb9f27679012c1c03fd0c2c6dac5
Vahab <myvahab@gmail.com>
2025-05-24 09:00:58 +0330

    Update vo-kavehnegar.php

--- vo_bankpay/lib/Senders/vo-kavehnegar.php
+++ vo_bankpay/lib/Senders/vo-kavehnegar.php
@@ -2,30 +2,27 @@
 
 function send_sms($params=[]){
     try {
-	    
-  	$from = $params['sendnumber'];
+
+	$from = $params['sendnumber'];
   	$to = $params['tonumbers'][0];
   	$message = $params['message'];
-  	$username = $params['username'];
-  	$password = $params['password'];
-  	$curl = curl_init();
-  	curl_setopt_array($curl, array(
-  	CURLOPT_URL => "http://api.kavenegar.com/v1/{$token}/sms/send.json",
-  	CURLOPT_RETURNTRANSFER => true,
-  	CURLOPT_ENCODING => '',
-  	CURLOPT_MAXREDIRS => 10,
-  	CURLOPT_TIMEOUT => 0,
-  	CURLOPT_FOLLOWLOCATION => true,
-  	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  	CURLOPT_CUSTOMREQUEST => 'POST',
-  	CURLOPT_POSTFIELDS => "receptor={$to}&sender={$from}&message={$message}",
-  	CURLOPT_HTTPHEADER => array(
-  		'Content-Type: application/x-www-form-urlencoded',
-  		'Cookie: cookiesession1=678A8C31FHJLMNOQRSTUV012345665A2'
-  	  ),
-  	));
-  	$response = curl_exec($curl);
-  	curl_close($curl);
+  	$username = $params['username'] ?: '';
+  	$password = $params['password'] ?: '';
+    	$apiKey = $username ?: $password;
+	    
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => "https://api.kavenegar.com/v1/{$apiKey}/sms/send.json?receptor={$to}&sender={$from}&message=" . urlencode($message),
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => '',
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => 'GET',
+	));
+	$response = curl_exec($curl);
+	curl_close($curl);
   	return json_encode([
   	    'Res' => $response,
   	    'Params' => $params,
